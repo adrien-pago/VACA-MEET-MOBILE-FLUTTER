9883c64bc526de762c8029c3c63fe01b2bb47f45
Adrien <115026138+adrien-pago@users.noreply.github.com>
2025-06-05 16:27:24 +0200

    up

--- vaca_meet_flutter/lib/pages/activity_camping_service.dart
+++ vaca_meet_flutter/lib/pages/activity_camping_service.dart
@@ -3,16 +3,18 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ActivityCampingService {
-  final String baseUrl = 'https://mobile.vaca-meet.fr/api/mobile/camping/info';
+  final String baseUrl = 'https://mobile.vaca-meet.fr/api/mobile/camping';
 
-  Future<Map<String, dynamic>?> getCampingInfo(int campingId) async {
+  Future<List<Map<String, dynamic>>> getActivities(int campingId, {required DateTime weekStart, required DateTime weekEnd}) async {
     try {
       final prefs = await SharedPreferences.getInstance();
       final token = prefs.getString('token');
       if (token == null) {
         throw Exception('Token JWT manquant');
       }
-      final url = Uri.parse('$baseUrl/$campingId');
+      final startStr = "${weekStart.year.toString().padLeft(4, '0')}-${weekStart.month.toString().padLeft(2, '0')}-${weekStart.day.toString().padLeft(2, '0')}";
+      final endStr = "${weekEnd.year.toString().padLeft(4, '0')}-${weekEnd.month.toString().padLeft(2, '0')}-${weekEnd.day.toString().padLeft(2, '0')}";
+      final url = Uri.parse('$baseUrl/$campingId/activities?start=$startStr&end=$endStr');
       final response = await http.get(
         url,
         headers: {
@@ -21,7 +23,12 @@ class ActivityCampingService {
         },
       );
       if (response.statusCode == 200) {
-        return jsonDecode(response.body) as Map<String, dynamic>;
+        final data = jsonDecode(response.body);
+        if (data is Map<String, dynamic> && data['activities'] is List) {
+          return List<Map<String, dynamic>>.from(data['activities']);
+        } else {
+          throw Exception('Format de réponse inattendu');
+        }
       } else {
         throw Exception('Erreur API: ${response.statusCode}');
       }
